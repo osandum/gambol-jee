64a4ffc9fc1a2a33f3b7f38a8ccfb4c7dfa8f04f
Ole Sandum <ole@sandum.net>
2014-07-12 18:10:50 +0200

    fumbling along, learning XSD as we go

--- src/test/java/gambol/xml/GambolParserTest.java
+++ src/test/java/gambol/xml/GambolParserTest.java
@@ -1,47 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package gambol.xml;
 
 import java.net.URL;
-import java.util.List;
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import org.junit.After;
-import org.junit.AfterClass;
 import static org.junit.Assert.*;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
 /**
  *
  * @author osa
  */
-public class JaxbParserTest {
+public class GambolParserTest {
 
-    JAXBContext jaxbContext;
-    
-    public JaxbParserTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+    JAXBContext jaxbContext;    
+    Schema schema;
+
     @Before
-    public void setUp() throws JAXBException {
+    public void setUp() throws JAXBException, SAXException {
         jaxbContext = JAXBContext.newInstance(Gambol.class);
 
         assertNotNull(jaxbContext);
+
+        SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        schema = sf.newSchema(getClass().getResource("/gambol.xsd"));
     }
     
     @After
@@ -53,12 +41,13 @@ public class JaxbParserTest {
         URL src = getClass().getResource("/sample/tournaments-2013_14.xml");
 
         Unmarshaller um = jaxbContext.createUnmarshaller();
+        um.setSchema(schema);
         Gambol cal = (Gambol)um.unmarshal(src);
         assertNotNull(cal);
 
         int n = 0;
-        for (Section s : cal.getSection())
-            for (Tournament t : s.getTournament()) 
+        for (Section s : cal.getSections())
+            for (Tournament t : s.getTournaments()) 
                 ++n;
         
         assertEquals(88, n);
@@ -73,11 +62,11 @@ public class JaxbParserTest {
         assertNotNull(cal);
         
         int n = 0;
-        for (Fixture f : cal.getTournament().getFixture()) {
+        for (Fixture f : cal.getTournament().getFixtures()) {
+            Side home = f.getSides().get(0);
+            Side away = f.getSides().get(1);
+            System.out.println(f.getStartTime() + ": " + home.getTeam() + "-" + away.getTeam() + " " + home.getScore() + "-" + away.getScore() + " (" + f.getId() +")");
             ++n;
-            for (Side s : f.getSide()) {
-                System.out.println(s);
-            }
         }
         
         assertEquals(56, n);
