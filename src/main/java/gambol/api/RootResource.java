2167efc8ace4fefa3e1456488db78d546a42e717
Ole Sandum <ole@sandum.net>
2014-08-27 08:46:44 +0200

    add 'X-PUBLISHED-TTL:PT5M' in a naive attempt to get Google to refresh more frequently

--- src/main/java/gambol/api/RootResource.java
+++ src/main/java/gambol/api/RootResource.java
@@ -27,6 +27,7 @@ import javax.ws.rs.core.UriInfo;
 import net.fortuna.ical4j.data.CalendarOutputter;
 import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.DateTime;
+import net.fortuna.ical4j.model.TimeZone;
 import net.fortuna.ical4j.model.TimeZoneRegistry;
 import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
 import net.fortuna.ical4j.model.ValidationException;
@@ -36,6 +37,7 @@ import net.fortuna.ical4j.model.property.CalScale;
 import net.fortuna.ical4j.model.property.Description;
 import net.fortuna.ical4j.model.property.Geo;
 import net.fortuna.ical4j.model.property.Location;
+import net.fortuna.ical4j.model.property.Method;
 import net.fortuna.ical4j.model.property.ProdId;
 import net.fortuna.ical4j.model.property.Summary;
 import net.fortuna.ical4j.model.property.Uid;
@@ -53,7 +55,7 @@ public class RootResource {
 
     @EJB
     App gambol;
-    
+
     @GET
     @Path("clubs")
     @Produces({APPLICATION_JSON, APPLICATION_XML})
@@ -64,10 +66,10 @@ public class RootResource {
         }
         return res;
     }
-    
+
     @Context
     UriInfo uriInfo;
-    
+
     @GET
     @Path("tournaments")
     @Produces({APPLICATION_JSON, APPLICATION_XML})
@@ -78,7 +80,7 @@ public class RootResource {
         }
         return Response.ok(res.toArray(new Tournament[0])).build();
     }
-    
+
     @GET
     @Path("fixtures")
     @Produces({APPLICATION_JSON, APPLICATION_XML})
@@ -91,18 +93,18 @@ public class RootResource {
             @QueryParam("club") List<String> clubRef,
             @QueryParam("home") List<String> homeClubRef,
             @QueryParam("away") List<String> awayClubRef) {
-        
-        FixturesQueryParam searchParams = qp(start, end, seasonId, seriesId, tournamentRef, clubRef, homeClubRef, awayClubRef);        
+
+        FixturesQueryParam searchParams = qp(start, end, seasonId, seriesId, tournamentRef, clubRef, homeClubRef, awayClubRef);
         List<FixtureEntity> fixtures = gambol.getFixtures(searchParams);
-        
+
         Fixtures res = new Fixtures();
         for (FixtureEntity entity : fixtures) {
             res.getFixtures().add(TournamentResource.entity2domain(entity));
         }
-        
+
         return Response.ok(res).build();
     }
-     
+
     private static Date u(DateParam p) {
         return p == null ? null : p.getValue();
     }
@@ -119,7 +121,7 @@ public class RootResource {
         res.setAwayClubRef(awayClubRef);
         return res;
     }
-    
+
     @GET
     @Path("events")
     @Produces(APPLICATION_JSON)
@@ -132,22 +134,22 @@ public class RootResource {
             @QueryParam("club") List<String> clubRef,
             @QueryParam("home") List<String> homeClubRef,
             @QueryParam("away") List<String> awayClubRef) throws ValidationException, IOException {
-        
-        FixturesQueryParam searchParams = qp(start, end, seasonId, seriesId, tournamentRef, clubRef, homeClubRef, awayClubRef);        
+
+        FixturesQueryParam searchParams = qp(start, end, seasonId, seriesId, tournamentRef, clubRef, homeClubRef, awayClubRef);
         List<FixtureEntity> fixtures = gambol.getFixtures(searchParams);
-        
+
         List<FullCalendarEvent> res = new LinkedList<FullCalendarEvent>();
         for (FixtureEntity f : fixtures) {
             FullCalendarEvent e = new FullCalendarEvent();
             e.id = f.getSourceRef(); // TODO: make our own opaque ref
-            e.title = f.getEventTitle(); 
+            e.title = f.getEventTitle();
             e.start = f.getStartTime();
             e.end = f.getEndTime();
             res.add(e);
         }
         return res;
     }
-    
+
     @GET
     @Path("fixtures.ics")
     @Produces("text/calendar; charset=UTF-8")
@@ -163,13 +165,13 @@ public class RootResource {
             @QueryParam("calname") String calname,
             @QueryParam("caldesc") String caldesc) throws ValidationException, IOException {
 
-        FixturesQueryParam searchParams = qp(start, end, seasonId, seriesId, tournamentRef, clubRef, homeClubRef, awayClubRef);        
+        FixturesQueryParam searchParams = qp(start, end, seasonId, seriesId, tournamentRef, clubRef, homeClubRef, awayClubRef);
         List<FixtureEntity> fixtures = gambol.getFixtures(searchParams);
 
         Calendar cal = getCalendar(fixtures, calname, caldesc);
-        
+
         cal.validate();
-        
+
         ByteArrayOutputStream bo = new ByteArrayOutputStream();
         new CalendarOutputter().output(cal, bo);
         bo.close();
@@ -182,25 +184,29 @@ public class RootResource {
             f += p + "-";
         for (String p : seasonId)
             f += p + "-";
-        
+
         return Response.ok(res)
                 .header("Content-Disposition", "attachment;filename=" + f + "fixtures.ics")
                 .build();
     }
-    
+
     protected Calendar getCalendar(List<FixtureEntity> fixtures, String calname, String caldesc) {
         TimeZoneRegistry tzr = TimeZoneRegistryFactory.getInstance().createRegistry();
-        VTimeZone tz = tzr.getTimeZone("Europe/Copenhagen").getVTimeZone();
-        
+        TimeZone tz = tzr.getTimeZone("Europe/Copenhagen");
+        VTimeZone vtz = tz.getVTimeZone();
+
         Calendar cal = new Calendar();
-        cal.getComponents().add(tz);
+        cal.getComponents().add(vtz);
         cal.getProperties().add(new ProdId("-//Gambol//iCal4j 1.0//EN"));
         cal.getProperties().add(Version.VERSION_2_0);
+        cal.getProperties().add(Method.PUBLISH);
         cal.getProperties().add(CalScale.GREGORIAN);
         if (calname != null)
-            cal.getProperties().add(new XProperty("X-WR-CALNAME", calname));        
+            cal.getProperties().add(new XProperty("X-WR-CALNAME", calname));
         if (caldesc != null)
-            cal.getProperties().add(new XProperty("X-WR-CALDESC", caldesc));        
+            cal.getProperties().add(new XProperty("X-WR-CALDESC", caldesc));
+        cal.getProperties().add(new XProperty("X-PUBLISHED-TTL", "PT5M"));
+
         int n = 0;
         for (FixtureEntity f : fixtures) {
             String eventName = f.getEventTitle();
@@ -208,24 +214,23 @@ public class RootResource {
             DateTime end = new DateTime(f.getEndTime());
             String eventDescr = f.getEventDescription();
             VEvent evt = new VEvent(start, end, eventName);
-            
+
             String uid = "GAMBOL:fixture:" + f.getSourceRef();
             evt.getProperties().add(new Uid(uid));
-            
+
             evt.getProperties().add(new Description(eventDescr));
-            
+
             ClubEntity homeClub = f.getHomeSide().getTeam().getClub();
-            if (!StringUtils.isEmpty(homeClub.getAddress())) 
+            if (!StringUtils.isEmpty(homeClub.getAddress()))
                 evt.getProperties().add(new Location(homeClub.getAddress()));
             if (homeClub.getLatitude() != null && homeClub.getLongitude() != null)
                 evt.getProperties().add(new Geo(homeClub.getLatitude(), homeClub.getLongitude()));
-            
+
             cal.getComponents().add(evt);
-            
+
             ++n;
         }
-        
+
         return cal;
-        
     }
 }
