ef9ab0a92a5a10ddb7e8fefe390847ba841c09e3
Ole Sandum <ole@sandum.net>
2016-10-02 13:18:06 +0200

    drop fixture person class inheritance

--- src/main/java/gambol/model/FixturePlayerEntity.java
+++ src/main/java/gambol/model/FixturePlayerEntity.java
@@ -1,18 +1,63 @@
 package gambol.model;
 
+import java.io.Serializable;
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 /**
  * @author osa
  */
-@Entity(name = "fixture_player")
-@DiscriminatorValue("PLAYER")
-public class FixturePlayerEntity extends FixturePersonEntity {
+@Entity(name = "fixture_person")
+@Table(uniqueConstraints = { 
+    @UniqueConstraint(name = "player_uq", 
+            columnNames={"fixture_id", "side_id", "person_id", "jerseynumber"}) })
+public class FixturePlayerEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_fixture"))
+    private FixtureEntity fixture;
+    
+    @ManyToOne(optional = false)
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_person"))
+    private PersonEntity person;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+
+    public FixtureEntity getFixture() {
+        return fixture;
+    }
+
+    public void setFixture(FixtureEntity fixture) {
+        this.fixture = fixture;
+    }
+
+
+    public PersonEntity getPerson() {
+        return person;
+    }
+
+    public void setPerson(PersonEntity person) {
+        this.person = person;
+    }    
 
     @ManyToOne(optional = false)
     @JoinColumn(foreignKey = @ForeignKey(name = "fk_side"))
@@ -71,8 +116,7 @@ public class FixturePlayerEntity extends FixturePersonEntity {
         return "{" + getId() + ":" + side + " " + jerseyNumber + " " + getPerson() + " "+line+":"+pos+"}";
     }
 
-    @Override
     public String signature() {
-        return super.signature() + ":PLAYER";
+        return String.format("%d:%d:PLAYER", getFixture().getId(), person.getId());
     }
 }
