42684a90e6c1ebb9291a0482f82d97b1e3b607ba
Ole Sandum <ole@sandum.net>
2023-10-29 23:55:09 +0100

    Wildfly 20.0.1 → 25.0.0

--- frontend/push-abode.sh
+++ frontend/push-abode.sh
@@ -2,9 +2,8 @@
 
 mvn clean install -DskipTests
 
-podman build --format docker -t gambol-wf20 --rm --squash . 
+podman build --format docker -t gambol-wf25 --rm --squash .
 
-podman push gambol-wf20 sandum.net:5000/osa/gambol:wf20
+podman push gambol-wf25 sandum.net:5000/osa/gambol:wf25
 
 # kubectl -n osa patch deployment gambol -p   "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
- 
