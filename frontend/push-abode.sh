684b19ac7126874d5f7cb0cb09ff796a411ed807
Ole Sandum <ole@sandum.net>
2024-06-02 18:48:03 +0200

    Compile with Java 11
    Dusted off project and bunped some deps to make it buildable

--- frontend/push-abode.sh
+++ frontend/push-abode.sh
@@ -4,6 +4,6 @@ mvn clean install -DskipTests
 
 podman build --format docker -t gambol-wf25 --rm --squash .
 
-podman push gambol-wf25 sandum.net:5000/osa/gambol:wf25
+podman push gambol-wf25 sandum.net:5000/osa/gambol:wf25-11
 
 # kubectl -n osa patch deployment gambol -p   "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
